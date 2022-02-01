b021abb8822dd2329d264e63dc650ace75eebc90
Dani Schuhman <dani8439@aol.com>
2022-02-01 16:04:48 -0500

    added note about redux.

--- time-app/src/index.js
+++ time-app/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+/// are we going to use Redux to store site state?
+// import { Provider } from 'react-redux'
+// import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
