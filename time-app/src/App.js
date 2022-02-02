c069490cbdd4454eb410be000c468a6663c274d3
Dani Schuhman <dani8439@aol.com>
2022-02-02 15:18:55 -0500

    Options menu.

--- time-app/src/App.js
+++ time-app/src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import cityTimeZone from "city-timezones";
 import axios from "axios";
 import Select from "react-select";
-import TimezoneSelect from "react-timezone-select";
 
 // const defaultInput = {
 //   city: ""
@@ -21,8 +20,107 @@ export default function App() {
   const [ColleguesEndTime, setColleguesEndTime] = useState("");
   const [result, setResult] = useState(null);
 
-  const [selectedTimezone, setSelectedTimezone] = useState({});
-  const [selectedTimezone2, setSelectedTimezone2] = useState({});
+  // timeZone
+  //   const [textInput, setTextInput] = useState(defaultInput);
+  //   const cityMapping = cityTimeZone.cityMapping;
+  //   const flatCityMap = (arr) => {
+  //     const flatObject = {};
+  //     for (let i = 0; i < arr.length; i++) {
+  //       for (const property in arr[i]) {
+  //         flatObject[`${property}_${i}`] = arr[i][property];
+  //       }
+  //     }
+  //     return flatObject;
+  //   };
+  //   console.log(cityMapping);
+  //   console.log(flatCityMap(cityMapping));
+
+  const options = [
+    { value: "Taulaga", label: "UTC-11" },
+    { value: "Avarua", label: "UTC-10" },
+    { value: "Taiohae", label: "UTC-9:30" },
+    { value: "Juneau", label: "UTC-9:00" },
+    { value: "Los Angeles", label: "UTC-8:00" },
+    { value: "Boise", label: "UTC-7:00" },
+    { value: "Dangriga", label: "UTC-6:00" },
+    { value: "Toronto", label: "UTC-5:00" },
+    { value: "Holetown", label: "UTC-4:00" },
+    { value: "Conception Bay South", label: "UTC-3:30" },
+    { value: "Buenos Aires", label: "UTC-3:00" },
+    { value: "Grytviken", label: "UTC-2:00" },
+    { value: "Praia", label: "UTC-1:00" },
+    { value: "London", label: "UTC-0:00" },
+    { value: "Algiers", label: "UTC+1:00" },
+    { value: "Cairo", label: "UTC+2:00" },
+    { value: "Asmara", label: "UTC+3:00" },
+    { value: "Tehran", label: "UTC+3:30" },
+    { value: "Muscat", label: "UTC+4:00" },
+    { value: "Kabul", label: "UTC+4:30" },
+    { value: "Salqar", label: "UTC+5:00" },
+    { value: "Negombo", label: "UTC+5:30" },
+    { value: "Kathmandu", label: "UTC+5:45" },
+    { value: "Karagandy", label: "UTC+6:00" },
+    { value: "Yangon", label: "UTC+6:30" },
+    { value: "Bangkok", label: "UTC+7:00" },
+    { value: "Hong Kong", label: "UTC+8:00" },
+    { value: "Caiguna", label: "UTC+8:45" },
+    { value: "Seoul", label: "UTC+9:00" },
+    { value: "Darwin", label: "UTC+9:30" },
+    { value: "Vladivostok", label: "UTC+10:00" },
+    { value: "Adelaide", label: "UTC+10:30" },
+    { value: "Buka", label: "UTC+11:00" },
+    { value: "Bilibino", label: "UTC+12:00" },
+    { value: "Waitangi", label: "UTC+12:45" },
+    { value: "Apia", label: "UTC+13:00" },
+    { value: "Tarawa", label: "UTC+14:00" }
+  ];
+
+  const options2 = [
+    { value: "Taulaga", label: "UTC-11" },
+    { value: "Avarua", label: "UTC-10" },
+    { value: "Taiohae", label: "UTC-9:30" },
+    { value: "Juneau", label: "UTC-9:00" },
+    { value: "Los Angeles", label: "UTC-8:00" },
+    { value: "Boise", label: "UTC-7:00" },
+    { value: "Dangriga", label: "UTC-6:00" },
+    { value: "Toronto", label: "UTC-5:00" },
+    { value: "Holetown", label: "UTC-4:00" },
+    { value: "Conception Bay South", label: "UTC-3:30" },
+    { value: "Buenos Aires", label: "UTC-3:00" },
+    { value: "Grytviken", label: "UTC-2:00" },
+    { value: "Praia", label: "UTC-1:00" },
+    { value: "London", label: "UTC-0:00" },
+    { value: "Algiers", label: "UTC+1:00" },
+    { value: "Cairo", label: "UTC+2:00" },
+    { value: "Asmara", label: "UTC+3:00" },
+    { value: "Tehran", label: "UTC+3:30" },
+    { value: "Muscat", label: "UTC+4:00" },
+    { value: "Kabul", label: "UTC+4:30" },
+    { value: "Salqar", label: "UTC+5:00" },
+    { value: "Negombo", label: "UTC+5:30" },
+    { value: "Kathmandu", label: "UTC+5:45" },
+    { value: "Karagandy", label: "UTC+6:00" },
+    { value: "Yangon", label: "UTC+6:30" },
+    { value: "Bangkok", label: "UTC+7:00" },
+    { value: "Hong Kong", label: "UTC+8:00" },
+    { value: "Caiguna", label: "UTC+8:45" },
+    { value: "Seoul", label: "UTC+9:00" },
+    { value: "Darwin", label: "UTC+9:30" },
+    { value: "Vladivostok", label: "UTC+10:00" },
+    { value: "Adelaide", label: "UTC+10:30" },
+    { value: "Buka", label: "UTC+11:00" },
+    { value: "Bilibino", label: "UTC+12:00" },
+    { value: "Waitangi", label: "UTC+12:45" },
+    { value: "Apia", label: "UTC+13:00" },
+    { value: "Tarawa", label: "UTC+14:00" }
+  ];
+
+  //   const handleChange = (e) => {
+  //     setTextInput({
+  //       ...textInput,
+  //       [e.target.name]: e.target.value
+  //     });
+  //   };
 
   const findRangeIntersection = (arr1 = [], arr2 = []) => {
     const [el11, el12] = arr1;
@@ -80,13 +178,31 @@ export default function App() {
   return (
     <div className='App'>
       {/* THE API FOR CITIES */}
-      <h3>Please make a selection</h3>
-      <div className='select-wrapper'>
-        <TimezoneSelect
-          value={selectedTimezone}
-          onChange={setSelectedTimezone}
-        />
+      <div>
+        <Select options={options} />
       </div>
+
+      {/* <main>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-group'>
+            <label htmlFor='city'>Select your City</label>
+            <div className='form-field select'>
+              <select
+                id='city'
+                name='city'
+                onChange={handleChange}
+                value={textInput.city}
+              >
+                <option value=''>None Selected</option>
+                <option value='moscow'>Moscow</option>
+                <option value='delhi'>Delhi</option>
+              </select>
+              <span className='focus'></span>
+            </div>
+          </div>
+          <button type='submit'>Submit</button>
+        </form>
+      </main> */}
       <div>
         my start time
         <input type='time' onChange={(e) => setMyStartTime(e.target.value)} />
@@ -95,13 +211,8 @@ export default function App() {
         my end time
         <input type='time' onChange={(e) => setMyEndTime(e.target.value)} />
       </div>
-
-      <h3>Please make a selection</h3>
-      <div className='select-wrapper'>
-        <TimezoneSelect
-          value={selectedTimezone2}
-          onChange={setSelectedTimezone2}
-        />
+      <div>
+        <Select options={options2} />
       </div>
       <div>
         collegue start time
