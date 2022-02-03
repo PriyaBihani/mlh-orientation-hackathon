8836af53925a4ae452b63000afcd5b21fdf10147
Katia Utochkina <ekaterina.utochkina.2@gmail.com>
2022-02-03 17:47:57 +0300

    [#1] Add styling to make using the app feels like a unified experience
    - Add app styling
    
    - Fix jsx in the App component
    
    - Add editorconfig file for the editing rules the project follows
    
    - Add prettier file to take care of code formatting

--- time-app/src/App.js
+++ time-app/src/App.js
@@ -1,94 +1,144 @@
-import { useState } from 'react';
-import cityTimeZone from 'city-timezones';
-import Select, { createFilter } from 'react-select';
+import { useState } from "react";
+import cityTimeZone from "city-timezones";
+import Select, { createFilter } from "react-select";
 
-import data from './data.json';
-import CustomOption from './components/CustomOption';
-import CustomMenuList from './components/CustomMenuList';
+import data from "./data.json";
+import CustomOption from "./components/CustomOption";
+import CustomMenuList from "./components/CustomMenuList";
 
-import { getTimezoneDifference, findRangeIntersection } from './helpers';
+import { getTimezoneDifference, findRangeIntersection } from "./helpers";
+
+const { options } = data;
 
 export default function App() {
-	const { options } = data;
+  const [myStartTime, setMyStartTime] = useState("");
+  const [myEndTime, setMyEndTime] = useState("");
+  const [colleaguesStartTime, setColleaguesStartTime] = useState("");
+  const [colleaguesEndTime, setColleaguesEndTime] = useState("");
+  const [result, setResult] = useState(null);
+  const [myCity, setMyCity] = useState("");
+  const [myColleaguesCity, setColleaguesCity] = useState("");
 
-	const [myStartTime, setMyStartTime] = useState('');
-	const [myEndTime, setMyEndTime] = useState('');
-	const [colleaguesStartTime, setColleaguesStartTime] = useState('');
-	const [colleaguesEndTime, setColleaguesEndTime] = useState('');
-	const [result, setResult] = useState(null);
-	const [myCity, setMyCity] = useState('');
-	const [myColleaguesCity, setColleaguesCity] = useState('');
+  const handleSubmit = async () => {
+    let [myTimezone = {}] = cityTimeZone.lookupViaCity(myCity);
+    let [colleagueTimezone = {}] = cityTimeZone.lookupViaCity(myColleaguesCity);
+    let offset = await getTimezoneDifference(colleagueTimezone.timezone, myTimezone.timezone);
+    let localColleagueStartTime = Number(colleaguesStartTime.split(":")[0]) + offset;
+    let localColleagueEndTime = Number(colleaguesEndTime.split(":")[0]) + offset;
 
-	const handleSubmit = async () => {
-		let [myTimezone = {}] = cityTimeZone.lookupViaCity(myCity);
-		let [colleagueTimezone = {}] = cityTimeZone.lookupViaCity(myColleaguesCity);
+    let resultString = ``;
 
-		let offset = await getTimezoneDifference(
-			colleagueTimezone.timezone,
-			myTimezone.timezone
-		);
+    const result = findRangeIntersection(
+      [Number(myStartTime.split(":")[0]), Number(myEndTime.split(":")[0])],
+      [localColleagueStartTime, localColleagueEndTime]
+    );
 
-		let localColleagueStartTime =
-			Number(colleaguesStartTime.split(':')[0]) + offset;
-		let localColleagueEndTime =
-			Number(colleaguesEndTime.split(':')[0]) + offset;
+    resultString = `${result[0]} hours - ${result[1]} hours`;
+    setResult(resultString);
+  };
 
-		let resultString = ``;
+  return (
+    <div className="color-primary font-base">
+      <main>
+        <article className="wrapper">
+          {result ? (
+            <div className="key-header splitter gap-top-700">
+              <h2 className="text-700">
+                Your common working hours:
+                <span>{result}</span>
+              </h2>
+            </div>
+          ) : (
+            <div className="key-header splitter gap-top-700">
+              <h2 className="text-700">Find your common working hours</h2>
+            </div>
+          )}
 
-		const result = findRangeIntersection(
-			[Number(myStartTime.split(':')[0]), Number(myEndTime.split(':')[0])],
-			[localColleagueStartTime, localColleagueEndTime]
-		);
-
-		resultString = `${result[0]} hours - ${result[1]} hours`;
-		setResult(resultString);
-	};
-
-	return (
-		<div className='App'>
-			<div>
-				<Select
-					onChange={(obj) => setMyCity(obj.value)}
-					options={options}
-					filterOption={createFilter({ ignoreAccents: false })}
-					components={{ Option: CustomOption, MenuList: CustomMenuList }}
-				/>
-			</div>
-			<div>
-				my start time
-				<input type='time' onChange={(e) => setMyStartTime(e.target.value)} />
-			</div>
-			<div>
-				my end time
-				<input type='time' onChange={(e) => setMyEndTime(e.target.value)} />
-			</div>
-
-			<div>
-				<Select
-					onChange={(obj) => setColleaguesCity(obj.value)}
-					options={options}
-					filterOption={createFilter({ ignoreAccents: false })}
-					components={{ Option: CustomOption, MenuList: CustomMenuList }}
-				/>
-			</div>
-			<div>
-				colleague start time
-				<input
-					type='time'
-					onChange={(e) => setColleaguesStartTime(e.target.value)}
-				/>
-			</div>
-			<div>
-				colleague end time
-				<input
-					type='time'
-					onChange={(e) => setColleaguesEndTime(e.target.value)}
-				/>
-			</div>
-			<button type='button' onClick={handleSubmit}>
-				Get Over lapping hours
-			</button>
-			<div>{result && result}</div>
-		</div>
-	);
+          <div className="splitter gap-top-600">
+            <section class="flow">
+              <form className="form">
+                <div className="form__field">
+                  <span className="form__field__label" for="time">
+                    My timezone
+                  </span>
+                  <Select
+                    onChange={(obj) => setMyCity(obj.value)}
+                    options={options}
+                    filterOption={createFilter({ ignoreAccents: false })}
+                    components={{ Option: CustomOption, MenuList: CustomMenuList }}
+                    className="form__field__input1"
+                  />
+                </div>
+                <div className="form__field">
+                  <label className="form__field__label" for="time">
+                    My start time:
+                  </label>
+                  <input
+                    className="form__field__input"
+                    type="time"
+                    onChange={(e) => setMyStartTime(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form__field">
+                  <label className="form__field__label" for="time">
+                    My end time:
+                  </label>
+                  <input
+                    className="form__field__input"
+                    type="time"
+                    onChange={(e) => setMyEndTime(e.target.value)}
+                    required
+                  />
+                </div>
+              </form>
+            </section>
+            <section>
+              <form className="form">
+                <div className="form__field">
+                  <span className="form__field__label" for="time">
+                    Teammate's timezone
+                  </span>
+                  <Select
+                    onChange={(obj) => setColleaguesCity(obj.value)}
+                    options={options}
+                    filterOption={createFilter({ ignoreAccents: false })}
+                    components={{ Option: CustomOption, MenuList: CustomMenuList }}
+                    className="form__field__input1"
+                  />
+                </div>
+                <div className="form__field">
+                  <label className="form__field__label" for="time">
+                    Teammate's start time
+                  </label>
+                  <input
+                    className="form__field__input"
+                    type="time"
+                    onChange={(e) => setColleaguesStartTime(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form__field">
+                  <label className="form__field__label" for="time">
+                    Teammate's end time
+                  </label>
+                  <input
+                    className="form__field__input"
+                    type="time"
+                    onChange={(e) => setColleaguesEndTime(e.target.value)}
+                    required
+                  />
+                </div>
+              </form>
+            </section>
+          </div>
+          <div class="btn-container">
+            <button type="button" onClick={handleSubmit} className="button radius">
+              Get overlapping hours
+            </button>
+          </div>
+        </article>
+      </main>
+    </div>
+  );
 }
