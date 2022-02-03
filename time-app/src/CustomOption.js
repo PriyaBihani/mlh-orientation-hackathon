267c52c014f29a344e798a2a09f14cf9f49e661e
PriyaBihani <priyabihani.official@gmail.com>
2022-02-03 12:19:21 +0530

    feat: Custom Menu list and custom option for react-select

---
+++ time-app/src/CustomOption.js
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react';
+import { components } from 'react-select';
+
+const CustomOption = ({ children, ...props }) => {
+	const { onMouseMove, onMouseOver, ...rest } = props.innerProps;
+	const newProps = { ...props, innerProps: rest };
+	return <components.Option {...newProps}>{children}</components.Option>;
+};
+
+export default CustomOption;
