9e33ca540a4450ef80b19b253d8e230489b70937
PriyaBihani <priyabihani.official@gmail.com>
2022-02-03 12:27:34 +0530

    chore: Refactoring the code

--- time-app/src/App.js
+++ time-app/src/App.js
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import cityTimeZone from 'city-timezones';
-import axios from 'axios';
 import Select, { createFilter } from 'react-select';
+
 import data from './data.json';
-import CustomOption from './CustomOption';
-import CustomMenuList from './CustomMenuList';
+import CustomOption from './components/CustomOption';
+import CustomMenuList from './components/CustomMenuList';
+
+import { getTimezoneDifference, findRangeIntersection } from './helpers';
 
 export default function App() {
-	const api_key = 'FAJIO7OXE21N';
 	const { options } = data;
 
 	const [myStartTime, setMyStartTime] = useState('');
@@ -17,14 +18,6 @@ export default function App() {
 	const [result, setResult] = useState(null);
 	const [myCity, setMyCity] = useState('');
 	const [myColleaguesCity, setColleaguesCity] = useState('');
-
-	const findRangeIntersection = (arr1 = [], arr2 = []) => {
-		const [el11, el12] = arr1;
-		const [el21, el22] = arr2;
-		const leftLimit = Math.max(el11, el21);
-		const rightLimit = Math.min(el12, el22);
-		return [leftLimit, rightLimit];
-	};
 
 	const handleSubmit = async () => {
 		let [myTimezone = {}] = cityTimeZone.lookupViaCity(myCity);
@@ -49,20 +42,6 @@ export default function App() {
 
 		resultString = `${result[0]} hours - ${result[1]} hours`;
 		setResult(resultString);
-	};
-
-	const getTimezoneDifference = async (from, to) => {
-		try {
-			const res = await axios.get(
-				'http://api.timezonedb.com/v2.1/convert-time-zone',
-				{ params: { key: api_key, from, to, format: 'json' } }
-			);
-
-			let offset = res.data.offset / 3600;
-			return offset;
-		} catch (error) {
-			console.log(error.message);
-		}
 	};
 
 	return (
---
+++ time-app/src/helpers.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const api_key = 'FAJIO7OXE21N';
+
+export const getTimezoneDifference = async (from, to) => {
+	try {
+		const res = await axios.get(
+			'http://api.timezonedb.com/v2.1/convert-time-zone',
+			{ params: { key: api_key, from, to, format: 'json' } }
+		);
+
+		let offset = res.data.offset / 3600;
+		return offset;
+	} catch (error) {
+		console.log(error.message);
+	}
+};
+
+export const findRangeIntersection = (arr1 = [], arr2 = []) => {
+	const [el11, el12] = arr1;
+	const [el21, el22] = arr2;
+	const leftLimit = Math.max(el11, el21);
+	const rightLimit = Math.min(el12, el22);
+	return [leftLimit, rightLimit];
+};
