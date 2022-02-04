e7bdbd22a8b7d3d0859e7f00e7daf9a00756a22d
PriyaBihani <priyabihani.official@gmail.com>
2022-02-04 19:54:12 +0530

    fix: Added https for deployment err

--- time-app/src/helpers.js
+++ time-app/src/helpers.js
@@ -1,30 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getTimezoneDifference = async (from, to) => {
-	try {
-		const res = await axios.get(
-			'http://api.timezonedb.com/v2.1/convert-time-zone',
-			{
-				params: {
-					key: process.env.REACT_APP_API_KEY,
-					from,
-					to,
-					format: 'json',
-				},
-			}
-		);
+  try {
+    const res = await axios.get("https://api.timezonedb.com/v2.1/convert-time-zone", {
+      params: {
+        key: process.env.REACT_APP_API_KEY,
+        from,
+        to,
+        format: "json",
+      },
+    });
 
-		let offset = res.data.offset / 3600;
-		return offset;
-	} catch (error) {
-		console.log(error.message);
-	}
+    let offset = res.data.offset / 3600;
+    return offset;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export const findRangeIntersection = (arr1 = [], arr2 = []) => {
-	const [el11, el12] = arr1;
-	const [el21, el22] = arr2;
-	const leftLimit = Math.max(el11, el21);
-	const rightLimit = Math.min(el12, el22);
-	return [leftLimit, rightLimit];
+  const [el11, el12] = arr1;
+  const [el21, el22] = arr2;
+  const leftLimit = Math.max(el11, el21);
+  const rightLimit = Math.min(el12, el22);
+  return [leftLimit, rightLimit];
 };
